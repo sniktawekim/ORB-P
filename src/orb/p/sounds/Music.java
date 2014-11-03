504339c35ee5c964a7c50be5b9905777f05a938a
Michael Watkins <sniktawekim@gmail.com>
2014-11-03 02:34:01 -0500

    added mute

--- src/orb/p/sounds/Music.java
+++ src/orb/p/sounds/Music.java
@@ -3,38 +3,48 @@ package orb.p.sounds;
 import java.io.IOException;
 
 import org.newdawn.easyogg.OggClip;
+
 /**
  * Proof of concept
- * 
+ *
  * @author blainezor
  *
  */
 public class Music {
-	private OggClip ogg;
-         
-	
-	/**
-	 * File must be inside of the music.jar
-	 * @param fileName
-	 */
-	public Music(String fileName)
-	{
-		try {
-			ogg = new OggClip(fileName + ".ogg");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}     
-	
-	/**
-	 * Play
-	 */
-	public void play()
-	{
-		//ogg.play();
-	}
-	public boolean isPlaying(){
-            return !ogg.stopped();
+
+    private OggClip ogg;
+    private boolean mute = false;
+
+    /**
+     * File must be inside of the music.jar
+     *
+     * @param fileName
+     */
+    public Music(String fileName) {
+        try {
+            ogg = new OggClip(fileName + ".ogg");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-	
+    }
+
+    /**
+     * Play
+     */
+    public void play() {
+        if (!mute) {
+            ogg.play();
+        }
+    }
+
+    public boolean isPlaying() {
+        return !ogg.stopped();
+    }
+
+    public void tMute() {
+        mute = !mute;
+        if (mute) {
+            ogg.stop();
+        }
+    }
 }
