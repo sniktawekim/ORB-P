b7fc3ba4fa9fd0685761ca099c91ad400db3e7bf
Michael Watkins <sniktawekim@gmail.com>
2015-01-13 01:08:10 -0500

    file arrangement

---
+++ src/orb/p/art/CHARArt.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package orb.p.art;
+
+/**
+ *
+ * @author Omnicis
+ */
+public class CHARArt {
+
+    
+    //TEST CHARACTER
+    public static final String TEST = "pics/hud/startwizard/";
+    
+    
+    //REDHEAD
+    public static final String REDHEAD = "pics/hud/startwizard/";
+    
+    //WHITE ROBOT
+    public static final String ROBOT_3 = "pics/hud/startwizard/";
+    
+    
+    
+    
+}
